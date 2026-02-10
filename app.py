b3b145f450b0c812d92f6e9492fb29a9c897e233
2024dc04135-ux <2024dc04135@wilp.bits-pilani.ac.in>
2026-02-10 23:32:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+st.title("Student Health Risk Prediction")
+
+uploaded_file = st.file_uploader("student_health_data.csv", type="csv")
+
+if uploaded_file:
+    data = pd.read_csv(uploaded_file)
+    st.write("Data Preview:", data.head())
+
+    model_choice = st.selectbox("Choose Model", ["Logistic Regression", "Decision Tree", "KNN", "Naive Bayes", "Random Forest", "XGBoost"])
+    
+    model = joblib.load(f"model/{model_choice.replace(' ', '_').lower()}.pkl")
+    
+    
+    data = data.drop(columns=["Student_ID"], errors="ignore")
+    data = data.dropna(axis=1, how="all")
+    
+    
+    X = data.drop("Health_Risk_Level", axis=1)
+    y = data["Health_Risk_Level"]
+    
+    
+    X = pd.get_dummies(X, drop_first=True)
+    X = X.fillna(0)
+    
+    y_pred = model.predict(X)
+    
+    st.write("Predictions:", y_pred[:10])
